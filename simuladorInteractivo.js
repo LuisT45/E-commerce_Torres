09255356fe39d8894fafa3d221f74f54543a467a
LuisTorres <lt60900@gmail.com>
2023-07-09 18:03:52 -0300

    Cierre de la segunda Pre-entrega

--- simuladorInteractivo.js
+++ simuladorInteractivo.js
@@ -1,7 +1,46 @@
-let producto1        = prompt('¿Qué desea comprar?')
-const precioProducto = 200 * 1.21 //Precio del producto * IVA 
-if(producto1 === " "){
-    alert('No ingresó ningún producto')
-}
-console.log('El producto selecccionado es:' + producto1)
-console.log('El precio del producto seleccionado es:' + precioProducto)
+const productosSeleccionados = [];
+
+
+function agregarAlCarrito(producto) {
+    productosSeleccionados.push(producto);
+    console.log("Producto agregado al carrito: " + producto.nombre);
+  }
+
+  while (true) {
+    let nombreProducto = prompt("¿Qué desea comprar?");
+    let precioProducto = prompt("Ingrese el precio del producto:");
+  
+    if (nombreProducto === "" || precioProducto === "") {
+      alert("No ingresó todos los datos del producto");
+      continue; // Si no se ingresaron todos los datos, se vuelve a solicitar
+    }
+  
+    const producto = {
+      nombre: nombreProducto,
+      precio: parseFloat(precioProducto),
+    };
+  
+    agregarAlCarrito(producto);
+  
+   
+    let confirmacion = confirm("¿Desea agregar más productos al carrito?");
+  
+    if (!confirmacion) {
+      break; // Si no se desea agregar más productos, se termina el ciclo
+    }
+  }
+
+
+  function calcularPrecioTotal() {
+    let precioTotal = 0;
+  
+    for (let producto of productosSeleccionados) {
+      const precioProducto = 200 * 1.21; // Precio del producto * IVA
+      precioTotal += precioProducto;
+    }
+  
+    return precioTotal;
+  }
+  
+  console.log("Productos seleccionados: " + productosSeleccionados);
+  console.log("Precio total: $" + calcularPrecioTotal().toFixed(2));
