ce37f665176ce7a55ed96c8aa8e8f131ea29d4d4
LuisTorres <lt60900@gmail.com>
2023-07-24 17:02:27 -0300

    Cierre de la tercera Pre-entrega

--- simuladorInteractivo.js
+++ simuladorInteractivo.js
@@ -1,46 +1,150 @@
-const productosSeleccionados = [];
 
+  //Carrito de compra
+  const btnCart = document.querySelector('.container-cart-icon');
+  const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
 
-function agregarAlCarrito(producto) {
-    productosSeleccionados.push(producto);
-    console.log("Producto agregado al carrito: " + producto.nombre);
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
+});
+
+/* ========================= */
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector('.container-items');
+
+// Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+
+productsList.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
+
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
+		};
+
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
+
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
+					return product;
+				}
+			});
+			allProducts = [...products];
+		} else {
+			allProducts = [...allProducts, infoProduct];
+		}
+
+		showHTML();
+	}
+});
+
+rowProduct.addEventListener('click', e => {
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
+		const title = product.querySelector('p').textContent;
+
+		allProducts = allProducts.filter(
+			product => product.title !== title
+		);
+
+		console.log(allProducts);
+
+		showHTML();
+	}
+});
+
+// Funcion para mostrar  HTML
+const showHTML = () => {
+	if (!allProducts.length) {
+		cartEmpty.classList.remove('hidden');
+		rowProduct.classList.add('hidden');
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
+	}
+
+	// Limpiar HTML
+	rowProduct.innerHTML = '';
+
+	let total = 0;
+	let totalOfProducts = 0;
+
+	allProducts.forEach(product => {
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
+
+		containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+		rowProduct.append(containerProduct);
+
+		total =
+			total + parseInt(product.quantity * product.price.slice(1));
+		totalOfProducts = totalOfProducts + product.quantity;
+	});
+
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;
+};
+
+// Función para establecer los datos del carrito desde un JSON
+const setCartDataFromJSON = (jsonData) => {
+  allProducts = JSON.parse(jsonData);
+  showHTML();
+};
+
+// Evento para guardar los datos del carrito en el localStorage al cerrar la página
+window.addEventListener('beforeunload', () => {
+  const cartDataJSON = getCartDataAsJSON();
+  localStorage.setItem('cartData', cartDataJSON);
+});
+
+// Evento para cargar los datos del carrito desde el localStorage al cargar la página
+window.addEventListener('DOMContentLoaded', () => {
+  const cartDataJSON = localStorage.getItem('cartData');
+  if (cartDataJSON) {
+    setCartDataFromJSON(cartDataJSON);
   }
+});
 
-  while (true) {
-    let nombreProducto = prompt("¿Qué desea comprar?");
-    let precioProducto = prompt("Ingrese el precio del producto:");
-  
-    if (nombreProducto === "" || precioProducto === "") {
-      alert("No ingresó todos los datos del producto");
-      continue; // Si no se ingresaron todos los datos, se vuelve a solicitar
-    }
-  
-    const producto = {
-      nombre: nombreProducto,
-      precio: parseFloat(precioProducto),
-    };
-  
-    agregarAlCarrito(producto);
-  
-   
-    let confirmacion = confirm("¿Desea agregar más productos al carrito?");
-  
-    if (!confirmacion) {
-      break; // Si no se desea agregar más productos, se termina el ciclo
-    }
-  }
-
-
-  function calcularPrecioTotal() {
-    let precioTotal = 0;
-  
-    for (let producto of productosSeleccionados) {
-      const precioProducto = 200 * 1.21; // Precio del producto * IVA
-      precioTotal += precioProducto;
-    }
-  
-    return precioTotal;
-  }
-  
-  console.log("Productos seleccionados: " + productosSeleccionados);
-  console.log("Precio total: $" + calcularPrecioTotal().toFixed(2));
